4463cffbdcf48016666a3fc6de0a9381dc5fbfaa
SLITH7777 <72797539+SLITH7777@users.noreply.github.com>
2020-11-24 04:39:56 -0500

    Add files via upload

--- pit12/pit12.py
+++ pit12/pit12.py
@@ -1,1 +1,21 @@
+chislo = int(input("введите число"))
+if chislo % 3 == 0:
+    print("делится на три")
+else:
+    print("не делится на три")
+if chislo < 0:
+    print("меньше нуля")
+if chislo == 0:
+    print("равно нулю")
+if chislo > 0:
+    print("больше нуля")
+
+
+
+
+
+
+
+
+
 
