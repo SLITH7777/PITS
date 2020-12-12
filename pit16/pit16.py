ed7efe48324c430b751a7689ca7cbb3d6ff5249d
SLITH7777 <72797539+SLITH7777@users.noreply.github.com>
2020-12-12 04:09:05 -0500

    Create pit16.py
    d

---
+++ pit16/pit16.py
@@ -0,0 +1,6 @@
+login = input("введите логин")
+password = input("введите пароль")
+if login =="ooops" and password =="1234":
+ print("добро пожаловать")
+else:
+ print("ПАРОЛЬ ИЛИ ЛОГИН ВВЫДЕНЫ НЕ ВЕРНО")
