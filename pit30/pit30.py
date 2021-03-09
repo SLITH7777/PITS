3dfeefc444d0a5b1b584d59b260f7d8b6c24d782
SLITH7777 <72797539+SLITH7777@users.noreply.github.com>
2021-03-09 05:52:04 -0500

    Update pit30.py

--- pit30/pit30.py
+++ pit30/pit30.py
@@ -1,1 +1,13 @@
+per = int(input("Введите число"))
+neSumma = ("не нашлось")
+for p in range(2,per):
+    if per%p == 0:
+      neSumma = "нашлось"
 
+
+
+if neSumma == "нашлось":
+   print("cocтавное число")
+if neSumma == "не нашлось":
+   print("простое число")
+  
